b40346e5cbdba7ba08c9c05c95887a0c4667da38
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-01-24 16:34:16 -0800

    implemented bootstrap in rails app (hopefully)

--- gccmdb/app/javascript/packs/application.js
+++ gccmdb/app/javascript/packs/application.js
@@ -9,6 +9,15 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+// maybe not these?
+//import { Application } from "stimulus" 
+//import { definitionsFromContext } from "stimulus/webpack-helpers"
+// end maybe not these?
+
+import "bootstrap"
+import "../stylesheets/application"
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
