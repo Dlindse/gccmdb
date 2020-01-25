774ab5c86323a89e25ac537619209c7a52af556e
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-01-24 16:47:22 -0800

    verified bootstrap implementation, created scratch controler with three actions/views

--- gccmdb/config/routes.rb
+++ gccmdb/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'scratch/back'
+  get 'scratch/homepage'
+  get 'scratch/one'
+
+  root 'scratch#back'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
